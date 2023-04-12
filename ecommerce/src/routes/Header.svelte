2b286ab302b11be1d296513a282d6a6c229badc6
Ronaldo Sandoval Zambrana <ronaldo.sandoval@fundacion-jala.org>
2023-04-12 07:08:16 -0400

    repo

---
+++ ecommerce/src/routes/Header.svelte
@@ -0,0 +1,32 @@
+<header>
+    <nav>
+        <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/products">Products</a></li>
+        <li><a href="/cart">Cart</a></li>
+        </ul>
+    </nav>
+</header>
+```
+<style>
+    header {
+        background: #eee;
+        padding: 1em;
+    }
+    nav {
+        display: flex;
+        justify-content: space-between;
+    }
+    ul {
+        display: flex;
+        list-style: none;
+        padding: 0;
+    }
+    li {
+        margin: 0 0.5em;
+    }
+    a {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
